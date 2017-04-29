3acd9855996b2f0d3035fa23e0148958d3424214
Colin Murtaugh <cmurtaugh@gmail.com>
2017-04-29 12:27:55 -0400

    bump version to v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='canvas-data-sdk',
-    version='0.1.0',
+    version='0.1.1',
     author='Colin Murtaugh',
     author_email='cmurtaugh@gmail.com',
     packages=find_packages(),
